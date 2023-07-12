7a2ba5515eafd513fae71ff20588d7b957fe8ef0
Daniel K. Franch <daniel.f@dataloop.ai>
2023-07-12 15:53:43 +0300

    adding generation tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,9 @@
 import unittest
 import dtlpy as dl
+import json
+import random
+import torch
+import numpy as np
 from models import (open_llama, dialogpt_large, dslim_bert_base_ner, facebook_detr_resnet_50_panoptic,
                     facebook_detr_resnet_101)
 from creation import package_creation
@@ -8,34 +12,74 @@ from transformers import (GPT2LMHeadModel, GPT2TokenizerFast, LlamaTokenizer, Ll
                           BertForTokenClassification, BertTokenizerFast, DetrForSegmentation, DetrFeatureExtractor,
                           DetrForObjectDetection)
 
-TOKEN = '<INSERT-TOKEN-MECHANISM>'
+SEED = 1337
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
 
 
 class MyTestCase(unittest.TestCase):
+    project: dl.Project = None
+    package: dl.Package = None
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(SEED)
         if dl.token_expired():
-            dl.login_token(TOKEN)
-        self.project = dl.projects.create("hugging_face_adapter_tests")
-        self.package = package_creation(self.project, "../model_adapter.py")
+            dl.login()
+        try:
+            cls.project = dl.projects.create("hugging_face_adapter_tests")
+        except dl.exceptions.Conflict:
+            print("Dummy project already exists")
+            cls.project = dl.projects.get("hugging_face_adapter_tests")
+        cls.package = package_creation(cls.project, "../model_adapter.py")
 
-    def tearDown(self) -> None:
-        self.project.delete(sure=True, really=True)
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.project.delete(sure=True, really=True)
         dl.logout()
 
     def test_open_llama(self):
+        random.seed(SEED)
+        np.random.seed(SEED)
+        torch.manual_seed(SEED)
         model = open_llama.model_creation(self.package)
         model_adapter = ModelAdapter(model)
+        model_adapter.hugging.model.config.seed = SEED
+        with open("./test_input.json", "r") as f:
+            inp = json.load(f)
+        ans = model_adapter.predict([inp])
         self.assertTrue(isinstance(model_adapter.hugging.model, LlamaForCausalLM))
         self.assertTrue(isinstance(model_adapter.hugging.tokenizer, LlamaTokenizer))
         self.assertTrue('open_llama' in model_adapter.hugging.model.name_or_path.lower())
+        self.assertEqual(
+            ans[0][0]['coordinates'],
+            "I'm not sure if this is a test or not.\nI'm not sure if this is a test or not. I'm not sure if this is a test or not. I'm not sure if this is a test or not. I'm not sure if this is a test or not. I'm not sure if this is a test or not. I'm not sure if this is a test or not. I"
+            )
+        self.assertAlmostEqual(
+            ans[0][0]['metadata']['user']['model']['confidence'],
+            0.7106203436851501, 3
+            )
 
     def test_dialogpt(self):
         model = dialogpt_large.model_creation(self.package)
         model_adapter = ModelAdapter(model)
+        model_adapter.hugging.model.config.seed = SEED
+        with open("./test_input.json", "r") as f:
+            inp = json.load(f)
+        ans = model_adapter.predict([inp])
         self.assertTrue(isinstance(model_adapter.hugging.model, GPT2LMHeadModel))
         self.assertTrue(isinstance(model_adapter.hugging.tokenizer, GPT2TokenizerFast))
         self.assertTrue('dialogpt' in model_adapter.hugging.model.name_or_path.lower())
+        self.assertEqual(
+            ans[0][0]['coordinates'],
+            "It's a test of our faith."
+            )
+        self.assertAlmostEqual(
+            ans[0][0]['metadata']['user']['model']['confidence'],
+            0.3476366400718689, 3
+            )
 
     def test_bert_base(self):
         model = dslim_bert_base_ner.create_model_entity(self.package)
