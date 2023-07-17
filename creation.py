bacf786069d636a01603f126970749f59bab3207
Or Shabtay <or@dataloop.ai>
2023-07-17 12:52:08 +0300

    public models

--- creation.py
+++ creation.py
@@ -15,8 +15,11 @@ def package_creation(project: dl.Project) -> dl.Package:
     package = project.packages.push(package_name='hugging-face',
                                     ignore_sanity_check=True,
                                     src_path=os.getcwd(),
+                                    is_global=True,
                                     package_type='ml',
                                     modules=[modules],
+                                    codebase=dl.GitCodebase(git_url='https://github.com/dataloop-ai-apps/huggingface-adapter.git',
+                                                            git_tag='v0.1.9'),
                                     service_config={
                                         'runtime': dl.KubernetesRuntime(pod_type=dl.INSTANCE_CATALOG_GPU_K80_S,
                                                                         autoscaler=dl.KubernetesRabbitmqAutoscaler(
@@ -30,6 +33,6 @@ def package_creation(project: dl.Project) -> dl.Package:
 
 if __name__ == "__main__":
     env = 'prod'
-    project_id = "<insert-project-id>"
+    project_id = "d7ac8ef8-aaa0-47de-a800-db5ae58ff917"
     dl.setenv(env)
     package = package_creation(dl.projects.get(project_id=project_id))
