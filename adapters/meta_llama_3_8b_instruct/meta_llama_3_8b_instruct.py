e32740e70b3ba2ba33a66d6cbca8f8e571c0ec0e
Daniel K. Franch <daniel.f@dataloop.ai>
2024-04-24 18:30:03 +0300

    Switching from regular llama3 to llama3 instruct

--- adapters/meta_llama_3_8b_instruct/meta_llama_3_8b_instruct.py
+++ adapters/meta_llama_3_8b_instruct/meta_llama_3_8b_instruct.py
@@ -9,7 +9,7 @@ logger = logging.getLogger("[LLaMa3]")
 
 class HuggingAdapter:
     def __init__(self, configuration):
-        model_path = configuration.get("model_path", "meta-llama/Meta-Llama-3-8B")
+        model_path = configuration.get("model_path", "meta-llama/Meta-Llama-3-8B-Instruct")
         torch_dtype = configuration.get("torch_dtype", "fp16")
         if torch_dtype == 'fp32':
             torch_dtype = torch.float32
@@ -28,6 +28,10 @@ class HuggingAdapter:
                                      },
                                  device_map=configuration.get("device", "cpu")
                                  )
+        self.terminators = [
+            self.pipeline.tokenizer.eos_token_id,
+            self.pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+            ]
         self.configuration = configuration
         self.configuration["model_entity"].artifacts.upload(configuration.get("cache_dir"), "*")
     
@@ -45,17 +49,29 @@ class HuggingAdapter:
             item_annotations = dl.AnnotationCollection()
             for prompt_key, prompt_content in prompts.items():
                 questions = list(prompt_content.values()) if isinstance(prompt_content, dict) else prompt_content
-
                 for question in questions:
                     if question["mimetype"] == dl.PromptType.TEXT:
+                        messages = [
+                            {"role": "system", "content": self.configuration.get("system_prompt", "")},
+                            {"role": "user", "content": question["value"]}
+                            ]
+                        template_prompt = self.pipeline.tokenizer.apply_chat_template(
+                            messages,
+                            tokenize=False,
+                            add_generation_prompt=True
+                            )
                         logger.info(f"User: {question['value']}")
-                        pipeline_output = self.pipeline(question["value"],
-                                                        max_new_tokens=self.configuration.get("max_new_tokens", 20))
-                        response = pipeline_output[0]["generated_text"][len(question['value']):]
+                        pipeline_output = self.pipeline(template_prompt,
+                                                        max_new_tokens=self.configuration.get("max_new_tokens", 20),
+                                                        eos_token_id=self.terminators,
+                                                        do_sample=True,
+                                                        temperature=self.configuration.get("temperature", 0.6),
+                                                        top_p=self.configuration.get("top_p", 0.9))
+                        response = pipeline_output[0]["generated_text"][len(template_prompt):]
                         logger.info("Response: {}".format(response))
                         item_annotations.add(annotation_definition=dl.FreeText(text=response), prompt_id=prompt_key,
                                              model_info={
-                                                 "name": "LLaMa3",
+                                                 "name": "LLaMa3-8b-Instruct",
                                                  "confidence": 1.0
                                              })
                     else:
