6ebe94642a8819b5a7da366a02561ba70cef47ce
Daniel K. Franch <daniel.f@dataloop.ai>
2024-05-09 18:01:32 +0300

    Adding Kosmos

---
+++ adapters/microsoft_kosmos_2/kosmos_2_patch14_224.py
@@ -0,0 +1,86 @@
+import json
+import PIL
+import dtlpy as dl
+import logging
+from transformers import AutoModel, AutoProcessor
+
+logger = logging.getLogger("[KOSMOS-2]")
+CAPTIONING_PROMPT = "Caption this image."
+
+
+class HuggingAdapter:
+    def __init__(self, configuration):
+        self.model_name = configuration.get("model_name")
+        self.device = configuration.get("device")
+        self.captioning = configuration.get("captioning", False)
+
+        self.model = AutoModel.from_pretrained("unum-cloud/uform-gen2-qwen-500m", trust_remote_code=True)
+        self.processor = AutoProcessor.from_pretrained("unum-cloud/uform-gen2-qwen-500m", trust_remote_code=True)
+        self.model.to(self.device)
+
+    def prepare_item_func(self, item: dl.Item):
+        buffer = json.load(item.download(save_locally=False))
+        prompts = buffer["prompts"]
+        ready_prompts = []
+        for prompt_key, prompt_content in prompts.items():
+            questions = list(prompt_content.values()) if isinstance(prompt_content, dict) else prompt_content
+            image_buffer = None
+            prompt_text = None
+            prompt_image_found = False
+            prompt_text_found = False
+            for prompt_part in questions:
+                if "image" in prompt_part["mimetype"] and not prompt_image_found:
+                    image_url = prompt_part["value"]
+                    item_id = image_url.split("/stream")[0].split("/items/")[-1]
+                    item = dl.items.get(item_id=item_id)
+                    image_buffer = item.download(save_locally=False)
+                    prompt_image_found = True
+                elif "text" in prompt_part["mimetype"] and not prompt_text_found:
+                    prompt_text = prompt_part["value"]
+                    prompt_text_found = True
+                else:
+                    print("Wrong type found in prompt")
+                # Break loop after all inputs received
+                if prompt_image_found and prompt_text_found:
+                    break
+
+            if prompt_image_found and prompt_text_found:
+                ready_prompts.append((prompt_key, image_buffer, prompt_text))
+            else:
+                raise ValueError(f"{prompt_key} is missing either an image or a text prompt.")
+
+        return ready_prompts
+
+    def train(self, data_path, output_path, **kwargs):
+        logger.info("Training not implemented yet")
+
+    def predict(self, batch, **kwargs):
+        annotations = []
+        for prompts in batch:
+            item_annotations = dl.AnnotationCollection()
+            for prompt_key, image_buffer, prompt_txt in prompts:
+                prompt = f"<grounding> {prompt_txt}"
+                inputs = self.processor(text=prompt, images=PIL.Image.open(image_buffer), return_tensors="pt")
+                generated_ids = self.model.generate(
+                    pixel_values=inputs["pixel_values"],
+                    input_ids=inputs["input_ids"],
+                    attention_mask=inputs["attention_mask"],
+                    image_embeds=None,
+                    image_embeds_position_mask=inputs["image_embeds_position_mask"],
+                    use_cache=True,
+                    max_new_tokens=128,
+                    )
+                generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+                processed_text, entities = self.processor.post_process_generation(generated_text)
+                response = processed_text[len(prompt_txt) - 1:]
+                print("Response: {}".format(response))
+                item_annotations.add(
+                    annotation_definition=dl.FreeText(text=response),
+                    prompt_id=prompt_key,
+                    model_info={
+                        'name': self.model_name,
+                        'confidence': 1.0
+                        }
+                    )
+            annotations.append(item_annotations)
+        return annotations
