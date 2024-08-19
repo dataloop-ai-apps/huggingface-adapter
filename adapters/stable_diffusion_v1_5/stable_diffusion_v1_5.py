ba5a76fdd18c9d571742273f657442dad082c832
OfirDataloopAI <ofir.g@dataloop.ai>
2024-08-19 18:15:37 +0300

    fixed bugs in pix2pix and stable diffusion

--- adapters/stable_diffusion_v1_5/stable_diffusion_v1_5.py
+++ adapters/stable_diffusion_v1_5/stable_diffusion_v1_5.py
@@ -68,13 +68,15 @@ class HuggingAdapter:
 
                 image_result.save(image_result_path)
                 dataset = dl.datasets.get(dataset_id=dataset_id)
-                result_item = dataset.items.upload(local_path=image_result_path,
-                                                   remote_path="stable_diffusion_v1_5_results")
+                result_item: dl.Item = dataset.items.upload(
+                    local_path=image_result_path,
+                    remote_path="stable_diffusion_v1_5_results"
+                )
                 os.remove(image_result_path)
 
                 stream_url = result_item.stream
                 item_annotations.add(
-                    annotation_definition=dl.RefImage(ref=stream_url, mimetype="image/png"),
+                    annotation_definition=dl.RefImage(ref=stream_url, mimetype=result_item.mimetype),
                     prompt_id=prompt_key,
                     model_info={
                         'name': self.model_name,
